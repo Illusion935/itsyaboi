7397de0095f472a776ee138d81dbb4509a02a0ff
Tom Goossens <tomrai@live.nl>
2018-09-03 16:00:50 +0200

    delete cards.py

--- cards.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-foo = input("Enter the cards in order and divide them by a comma: ")
-print (foo)
-cards = foo.split(',')
-for i in range(0,len(cards)):
-    if (cards[i].isdigit()):
-        print("het werkt")
